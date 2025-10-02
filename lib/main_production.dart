36668cc86e5cabfa7d6ae939fd490fe55310d930
Mohamed0Amr <140004433+Mohamed0Amr@users.noreply.github.com>
2025-10-02 06:41:26 +0300

    -Add .vscode for the main production and main development -Add the package flutter flavors -Delete the main

---
+++ lib/main_production.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quraankareem/core/routing/app_router.dart';
+import 'package:quraankareem/quraan_app.dart';
+
+void main() async {
+  // To fix text being hidden bug in flutter_screenutil in release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(QuraanApp(appRouter: AppRouter()));
+}
